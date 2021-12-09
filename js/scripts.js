3f2d8008153a67dd2c74a45fd32d02d4e76a8f20
Eunice Otieno <otieunice@gmail.com>
2021-12-09 09:03:56 +0300

    practice on jquery

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+jQuery('h1').click(function(){
+    alert("This is a heading tag")
+  });
+
+  jQuery('p').click(function(){
+    alert("This is a paragraph")
+  });
+
+  jQuery('img').click(function(){
+    alert("This is an image")
+  });
+  $(document).ready(function() {
+    $("p").click(function() {
+      $("img").show();
+    });
+  });
